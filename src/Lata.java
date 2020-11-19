da5b5cc034b146036540582a2ba98ecccb71a57a
thematius <thematius.monsalve@gmail.com>
2020-11-19 14:26:31 -0500

    Agregando la clase Lata.

---
+++ src/Lata.java
@@ -0,0 +1,12 @@
+// import java.util.Date;
+
+public class Lata {
+    private String fechaCaducidad;
+    private int estado, numLote;
+
+    public Lata(String fechaCaducidad, int estado, int numLote) {
+        this.fechaCaducidad = fechaCaducidad;
+        this.estado = estado;
+        this.numLote = numLote;
+    }    
+}
